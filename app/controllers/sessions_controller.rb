7de5b2ea17b9fae103e017d5fded5d80585af5b9
Danielly Hernandes <danielly.hernandes@kellogg.northwestern.edu>
2023-05-30 21:13:37 +0000

    auth-partially-done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,37 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
+    # authenticate the user
+      #find user by email
+      @user = User.find_by({"email" => params["email"]})
+      #case true (user is found =1) - (if exists, it won't return empty)
+      if @user
+        #Check is password matches --> if matches, store the user's id in a secure cookie and redirect to "/posts" with a fash message
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+           session["user_id"] = @user["id"]
+           flash["notice"] = "Welcome, #{@user["first_name"]}!"
+           redirect_to "/posts" 
+        #if authentication fails: redirect to /login with a flash message
+        else
+           flash["notice"] = "Your password is not correct"
+           redirect_to "/login"
+        end  
+      #case no user is found:  
+      else
+        flash["notice"] = "No user found"
+        redirect_to "/login"  
+      end  
+        
   end
+
+  #code to logout an user:
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
+  end
+
 end
+
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
-    @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
-    @user.save
+    #check if the user exists
+    @user = User.find_by({"email" => params["user"]["email"]})
+    #case true (=1) - (if exists, it won't return empty)
+    if @user
+      flash["notice"] = "This email already exists"
+      redirect_to "/users/new"
+    #case false (=0) - (if it does not exist, it will return empty)
+    else
+      @user = User.new
+      @user["first_name"] = params["user"]["first_name"]
+      @user["last_name"] = params["user"]["last_name"]
+      @user["email"] = params["user"]["email"]
+      @user["password"] = BCrypt::Password.create(params["user"]["password"])
+      @user.save
     redirect_to "/posts"
+    end
   end
 end
