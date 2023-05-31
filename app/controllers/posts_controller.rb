e4cfdade2f35136df650160b8b5d9401e69372c1
Danielly Hernandes <danielly.hernandes@kellogg.northwestern.edu>
2023-05-31 17:45:11 +0000

    tacogram-auth-done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,28 @@ class PostsController < ApplicationController
     end
   
     def new
-      @post = Post.new
+      if @current_user
+        @post = Post.new
+      else
+        flash["notice"] = "You must login to post"
+        redirect_to "/login"
+      end
     end
   
     def create
-      @post = Post.new
-      @post["body"] = params["post"]["body"]
-      @post["image"] = params["post"]["image"]
-      @post.save
-      redirect_to "/posts"
+      #case true (=1) - let the user create the post
+      if @current_user
+        @post = Post.new
+        @post["body"] = params["post"]["body"]
+        @post["image"] = params["post"]["image"]
+        @post["user_id"] = @current_user["id"]
+        @post.save
+        redirect_to "/posts"       
+      #case false, show a message
+      else
+        flash["notice"] = "You must login to post"
+        redirect_to "/login"  
+      end
     end
   
   end
